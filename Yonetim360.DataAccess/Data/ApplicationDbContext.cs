a5b5bc806c7ede238a7f9a879e4932f3d4c24262
Talat <talatbahar5@gmail.com>
2025-07-25 18:34:34 +0300

    Global filter and savechangesAsync is completed

--- Yonetim360.DataAccess/Data/ApplicationDbContext.cs
+++ Yonetim360.DataAccess/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Yonetim360.DataAccess.Extensions;
 using Yonetim360.DataAccess.Services;
 using Yonetim360.Entity;
 
@@ -24,6 +25,11 @@ namespace Yonetim360.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplySoftDeleteFilter();
+
+
             // Tüm tenant entity'ler için otomatik filter
             foreach (var entityType in modelBuilder.Model.GetEntityTypes())
             {
@@ -36,6 +42,35 @@ namespace Yonetim360.DataAccess.Data
                     method.Invoke(null, new object[] { modelBuilder, _tenantProvider });
                 }
             }
+        }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            foreach (var entry in ChangeTracker.Entries<ITenantEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.TenantId = _tenantProvider.TenantId;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Property(x => x.TenantId).IsModified = false;
+                }
+            }
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.Entity.IsDeleted = true;
+                        entry.Entity.UpdatedAt = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.UpdatedAt = DateTime.UtcNow;
+                        break;
+                }
+            }
+            return await base.SaveChangesAsync(cancellationToken);
         }
 
         private static void SetTenantFilter<TEntity>(ModelBuilder modelBuilder, ITenantProvider tenantProvider)
