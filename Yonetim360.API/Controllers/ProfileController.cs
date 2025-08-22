e411de2d7da8e04dd3b2499ae334efa04f25aa79
Talat <talatbahar5@gmail.com>
2025-08-22 20:59:40 +0300

    .

---
+++ Yonetim360.API/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yonetim360Business.CQRS.Profiles.Queries.GetCurrentProfile;
+
+namespace Yonetim360.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : BaseController
+    {
+
+        [Authorize]
+
+        [HttpGet("me")]
+        public async Task<IActionResult> Me()
+        {
+            var result = await Mediator.Send(new GetCurrentProfileQuery());
+            return Ok(result);
+        }
+    }
+}
